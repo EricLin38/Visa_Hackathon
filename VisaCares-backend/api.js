91277d9b0687039760937c81bdb824b0f18264c4
Eric Lin <45245999+EricLin38@users.noreply.github.com>
2022-05-02 12:36:41 -0500

    Add files via upload

---
+++ VisaCares-backend/api.js
@@ -0,0 +1,12 @@
+// let express = require("express");
+
+// let apiRouter = express.Router();
+
+
+// let donationController = require("./controller/donationController")
+
+
+// apiRouter.post("/donate", donationController.donateProcess);
+
+
+// module.exports = apiRouter;
---
+++ VisaCares-backend/app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+
+// let apiRouter = require("./api");
+// app.use('/api', apiRouter);
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    next();
+});
+
+//let donationService = require('./controller/donationController');
+//donationService(app)
+
+let visaCheckoutService = require('./controller/visaCheckoutController');
+visaCheckoutService(app);
+let visauserValidateService = require('./controller/userInfoController');
+visauserValidateService(app);
+
+let taxformService = require('./controller/taxformController');
+taxformService(app);
+
+
+app.listen(3000, function () {
+    console.log("Example app listening on port 3000!");
+});
