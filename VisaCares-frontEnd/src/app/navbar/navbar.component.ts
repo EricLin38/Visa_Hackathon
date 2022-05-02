12f649f4179bbc054a70951d3d4cd33bfe721732
Eric Lin <45245999+EricLin38@users.noreply.github.com>
2022-05-02 12:27:23 -0500

    Add files via upload

---
+++ VisaCares-frontEnd/src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountServices } from '../services/accountServices';
+import { MatDialog } from '@angular/material/dialog';
+import { SigninComponent } from '../account/signin/signin.component';
+import { RegisterComponent } from '../account/register/register.component';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    public accountServices: AccountServices,
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    if(this.accountServices.login == false) { this.openLogin(); }
+  }
+
+  openLogin() {
+    const dialogRef = this.dialog.open(SigninComponent, {
+      maxWidth: '600px'
+    });
+  }
+
+  openRegister() {
+    this.dialog.open(RegisterComponent, {
+      maxWidth: '600px'
+    })
+  }
+}
